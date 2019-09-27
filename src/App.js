e1e86a7107c889c8c284e9519901d9eaac8f1ca6
Andre Praeiro <andre.campos@sigma.com.br>
2019-09-27 15:10:58 -0400

    config reactotron with redux

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import Routes from './routes';
-import Header from './components/Header';
-import store from './store';
+import './config/reactotronConfig';
 
+import Routes from './routes';
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+import store from './store';
 
 function App() {
   return (
