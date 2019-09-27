9a970d610e02e6b5bf41fa4c72db92a8ff32c94f
Andre Praeiro <andre.campos@sigma.com.br>
2019-09-27 11:15:44 -0400

    search products in api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,102 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/26/HZM-0953-226/HZM-0953-226_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/26/HZM-0953-226/HZM-0953-226_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/26/HZM-0953-226/HZM-0953-226_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/26/HZM-0953-226/HZM-0953-226_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
+    this.setState({ products: data });
+  }
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/26/HZM-0953-226/HZM-0953-226_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="fff" /> 3
+              </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/26/HZM-0953-226/HZM-0953-226_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
