78cb4459c2774b22dd8eb363d96e80cb7aef89b1
Andre Praeiro <andre.campos@sigma.com.br>
2019-09-27 10:32:02 -0400

    shopping basket estylization

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,66 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2/26/HZM-0953-226/HZM-0953-226_detalhe1.jpg?resize=280:280"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 250,58</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1.955,25</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
