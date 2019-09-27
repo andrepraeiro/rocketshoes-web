ba8972da4d9dee583d06c02e7bb19eaa7fb35f0b
Andre Praeiro <andre.campos@sigma.com.br>
2019-09-27 16:16:07 -0400

    refactoring actions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { bindActionCreators } from 'redux';
+
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
+
 import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
@@ -24,12 +29,9 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
+    const { addToCart } = this.props;
 
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    addToCart(product);
   };
 
   render() {
@@ -57,5 +59,10 @@ class Home extends Component {
     );
   }
 }
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
 
-export default connect()(Home);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Home);
